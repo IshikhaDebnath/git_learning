c78f8500532ebc01a027d89706c16160a6ec363b
Ishikha Debnath <ishikhadebnath19@gmail.com>
2025-07-15 00:02:10 +0530

    Ishikha

--- Ishikha.java
+++ Ishikha.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class Ishikha {
     public static void main(String[] args) {
-        System.out.println("🎉 I’m Ishika!");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("Adult");
+
+        }else {
+            System.out.println("Not Adult");
+        }
+       // System.out.println("🎉 I’m Ishika!")
     }
 }
